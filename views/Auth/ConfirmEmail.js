01a71fc60f5c580135e1a66588bc1d430fe57715
Maxime Tournier <mtournier@devsdefrance.com>
2024-03-21 15:27:50 +0100

    dark mode

--- views/Auth/ConfirmEmail.js
+++ views/Auth/ConfirmEmail.js
@@ -1,40 +1,55 @@
-import React from 'react'
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
-import { useTranslation } from 'react-i18next'
-import Message from '@mod/mobile-common/lib/components/utils/Message'
-import tw from 'twrnc'
+import React from "react"
+import { View, Text, TextInput, TouchableOpacity } from "react-native"
+import { useTranslation } from "react-i18next"
+import Message from "@mod/mobile-common/lib/components/utils/Message"
+import tw from "twrnc"
 import useOnChange from "@mod/mobile-common/lib/hooks/utils/useOnChange"
-import useHandleConfirmEmail from '../../lib/hooks/auth/useHandleConfirmEmail'
+import useHandleConfirmEmail from "../../lib/hooks/auth/useHandleConfirmEmail"
 import SuccessOrError from "@mod/mobile-common/lib/components/utils/SuccessOrError"
+import { useSelector } from "react-redux"
+import { useDynamicThemeStyles } from "@mod/mobile-common/styles/theme"
 
 const ConfirmEmail = ({ route }) => {
   const { userId } = route.params
 
-  const { handleConfirmEmail, data, setData, message } = useHandleConfirmEmail({ userId })
+  const { handleConfirmEmail, data, setData, message } = useHandleConfirmEmail({
+    userId,
+  })
 
   const { onChange } = useOnChange({ data, setData })
 
   const { t } = useTranslation()
 
+  const darkMode = useSelector((state) => state.theme.darkMode)
+  const { background, text } = useDynamicThemeStyles(darkMode)
+
   return (
-    <View style={tw`bg-white p-4 rounded-md h-full`}>
-      <Message message={t('actions.anEmailHasBeenSentToYouContainingA6DigitCode')} priority={"info"} />
-      <Text style={tw`font-medium text-lg mt-2`}>{t('utils.code')}</Text>
+    <View style={tw`${background} p-4 rounded-md h-full`}>
+      <Message
+        message={t("actions.anEmailHasBeenSentToYouContainingA6DigitCode")}
+        priority={"info"}
+      />
+      <Text style={tw`font-medium text-lg mt-2 ${text}`}>
+        {t("utils.code")}
+      </Text>
       <TextInput
-        style={tw`mt-2 px-3 py-2 text-gray-500 text-lg border border-slate-200 rounded-lg`}
-        placeholder={t('utils.code')}
-        onChangeText={(value) => onChange({ name: 'verificationCode', value: Number(value) })}
+        style={tw`mt-2 px-3 py-2 text-gray-500 text-lg bg-slate-100 rounded-md`}
+        placeholder={t("utils.code")}
+        onChangeText={(value) =>
+          onChange({ name: "verificationCode", value: Number(value) })
+        }
         value={data.verificationCode}
-        keyboardType='numeric'
+        keyboardType="numeric"
         maxLength={6}
       />
-        <TouchableOpacity style={tw`flex-row justify-center my-4 bg-indigo-600 rounded-lg`} onPress={handleConfirmEmail}>
-          <Text
-            style={tw`px-4 py-2 text-white text-xl font-medium`}
-          >
-            {t('utils.confirm')}
-          </Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        style={tw`flex-row justify-center my-4 bg-indigo-600 rounded-lg`}
+        onPress={handleConfirmEmail}
+      >
+        <Text style={tw`px-4 py-2 text-white text-xl font-medium`}>
+          {t("utils.confirm")}
+        </Text>
+      </TouchableOpacity>
 
       <SuccessOrError message={message} />
     </View>
--- views/Auth/Login.js
+++ views/Auth/Login.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react"
 import {
   View,
   TextInput,
@@ -7,15 +7,16 @@ import {
   ScrollView,
   ActivityIndicator,
   Image,
-} from 'react-native'
-import { useNavigation } from '@react-navigation/native'
-import { useTranslation } from 'react-i18next'
-import tw from 'twrnc'
-import GoogleSVG from '../../../../assets/images/GoogleSVG'
-import Message from '@mod/mobile-common/lib/components/utils/Message'
-import useHandleLogin from '../../lib/hooks/auth/useHandleLogin'
-import useHandleAuthGoogle from '../../lib/hooks/auth/useHandleAuthGoogle'
-import { useSelector } from 'react-redux'
+} from "react-native"
+import { useNavigation } from "@react-navigation/native"
+import { useTranslation } from "react-i18next"
+import tw from "twrnc"
+import GoogleSVG from "../../../../assets/images/GoogleSVG"
+import Message from "@mod/mobile-common/lib/components/utils/Message"
+import useHandleLogin from "../../lib/hooks/auth/useHandleLogin"
+import useHandleAuthGoogle from "../../lib/hooks/auth/useHandleAuthGoogle"
+import { useSelector } from "react-redux"
+import { useDynamicThemeStyles } from "@mod/mobile-common/styles/theme"
 
 const LoginScreen = () => {
   const navigation = useNavigation()
@@ -31,69 +32,77 @@ const LoginScreen = () => {
 
   const loading = useSelector((state) => state.auth.loading)
 
+  const darkMode = useSelector((state) => state.theme.darkMode)
+  const { background, text } = useDynamicThemeStyles(darkMode)
+
   const handleForgetPassword = () => {
-    navigation.navigate('ForgetPassword')
+    navigation.navigate("ForgetPassword")
   }
 
   return (
-    <ScrollView style={tw`flex bg-white h-full`}>
+    <ScrollView style={tw`flex ${background} h-full`}>
       <View style={tw`flex-col items-center`}>
         {isProcessing ? (
           <View style={tw`flex justify-center items-center`}>
-            <ActivityIndicator size={'large'} />
-            <Message priority={"info"} message={"Wait we are connecting to your Google account"} />
+            <ActivityIndicator size={"large"} />
+            <Message
+              priority={"info"}
+              message={"Wait we are connecting to your Google account"}
+            />
           </View>
         ) : (
           <Fragment>
-            <View style={tw`mt-5`}>
-              <View style={Platform.OS === 'ios' ? tw`mt-4` : null}>
+            <View style={tw`mt-5 items-center`}>
+              <View style={Platform.OS === "ios" ? tw`mt-4` : null}>
                 <Image
-                  style={[tw`w-30 h-15`, { resizeMode: 'contain' }]}
-                  source={require('../../../../assets/images/videotek_logo.webp')}
+                  style={[tw`w-30 h-15`, { resizeMode: "contain" }]}
+                  source={require("../../../../assets/images/videotek_logo.webp")}
                 />
               </View>
-              <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+              <TouchableOpacity onPress={() => navigation.navigate("Register")}>
                 <Text
-                  style={tw`font-medium text-base text-center my-2 text-indigo-600`}
+                  style={tw`font-medium text-base text-center my-2 text-indigo-600 ${text}`}
                 >
-                  {t('utils.notRegistered')}
+                  {t("utils.notRegistered")}
                 </Text>
               </TouchableOpacity>
             </View>
 
             <View style={tw`w-10/12`}>
-              <Text style={tw`font-medium text-lg`}>{t('utils.email')}</Text>
-              <TextInput
-                placeholder={t('utils.email')}
-                onChangeText={(text) => setData({ ...data, email: text })}
-                value={data.email}
-                style={tw`mt-2 px-3 py-2 text-gray-500 text-lg border border-slate-200 rounded-lg`}
-              />
-              <Text style={tw`font-medium text-lg mt-2`}>
-                {t('utils.password')}
+              <Text style={tw`font-medium text-lg ${text}`}>
+                {t("utils.email")}
               </Text>
               <TextInput
-                placeholder={t('utils.password')}
+                placeholder={t("utils.email")}
+                onChangeText={(text) => setData({ ...data, email: text })}
+                value={data.email}
+                style={tw`mt-2 px-3 py-2 text-gray-500 text-lg bg-slate-100 rounded-md`}
+              />
+              <Text style={tw`font-medium text-lg mt-2 ${text}`}>
+                {t("utils.password")}
+              </Text>
+              <TextInput
+                placeholder={t("utils.password")}
                 onChangeText={(text) => setData({ ...data, password: text })}
                 value={data.password}
                 secureTextEntry={true}
-                style={tw`mt-2 px-3 py-2 text-gray-500 text-lg border border-slate-200 rounded-lg`}
+                style={tw`mt-2 px-3 py-2 text-gray-500 text-lg bg-slate-100 rounded-md`}
               />
 
               {message.error ? (
-                <Message priority={'error'} message={message.error} />
+                <Message priority={"error"} message={message.error} />
               ) : null}
 
               {googleMessage.error ? (
-                <Message priority={'error'} message={googleMessage.error} />
+                <Message priority={"error"} message={googleMessage.error} />
               ) : null}
 
               <View>
                 <TouchableOpacity onPress={handleForgetPassword}>
                   <Text
-                    style={tw`mt-4 text-base font-medium text-indigo-600 text-center`}
+                    style={tw`mt-4 text-base font-medium text-indigo-600 text-center ${text}`}
                   >
-                    {t('utils.forgotYourPassword')}
+                    {t("utils.forgotYourPassword")}
                   </Text>
                 </TouchableOpacity>
               </View>
@@ -103,10 +112,10 @@ const LoginScreen = () => {
                 onPress={handleLogin}
               >
                 {loading ? (
-                  <ActivityIndicator size={'large'} />
+                  <ActivityIndicator size={"large"} />
                 ) : (
                   <Text style={tw`px-4 py-2 text-white text-xl font-medium`}>
-                    {t('utils.signIn')}
+                    {t("utils.signIn")}
                   </Text>
                 )}
               </TouchableOpacity>
@@ -121,7 +130,7 @@ const LoginScreen = () => {
               >
                 <GoogleSVG />
                 <Text style={tw`ml-2`}>
-                  {t('utils.continueWithGoogle')} Google
+                  {t("utils.continueWithGoogle")} Google
                 </Text>
               </TouchableOpacity>
             </View>
